5c6d7685eb4efca536006bdeb48c18a4cfc0c11f
M3dicCookie <pred102@hotmail.com>
2026-06-27 19:22:14 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,16 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('reader-store').then((cache) => {
-      return cache.addAll(['/', '/index.html', '/style.css', '/script.js', '/WIcon.png']);
+      return cache.addAll([
+        '/SpiritualCaress/',
+        '/SpiritualCaress/index.html',
+        '/SpiritualCaress/style.css',
+        '/SpiritualCaress/script.js',
+        '/SpiritualCaress/WIcon.png'
+      ]);
     })
   );
 });
-
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
